5b685974fd5c195e34040e8cb004bed7ea8c9bcd
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-10-19 11:26:22 +0900

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -2,7 +2,7 @@
 Author: Yosiyoshi
 """
 txt="Dit is een boek."
-h=[0,0,0,0,0,0,0]
+h=[0,0,0,0,0,0,0,0]
 h[0]=txt.count("oo")
 h[1]=txt.count("y")
 h[2]=txt.count("oe")
@@ -10,10 +10,11 @@ h[3]=txt.count("u")
 h[4]=txt.count("ie")
 h[5]=txt.count("ee")
 h[6]=txt.count("ou")
+h[7]=txt.count("uu")
 print("txt:", txt)
-print("oo:y:oe:u:ie:ee:ou existing in the text:", h)
+print("oo:y:oe:u:ie:ee:ou:uu existing in the text:", h)
 if max(h)==h[0]:
-    print("English?")
+    print("English or Dutch?")
 if max(h)==h[1]:
     print("English?")
 if max(h)==h[2]:
@@ -23,6 +24,8 @@ if max(h)==h[3]:
 if max(h)==h[4]:
     print("French or German?")
 if max(h)==h[5]:
-    print("Dutch?")
+    print("English or Dutch?")
 if max(h)==h[6]:
     print("French?")
+if max(h)==h[7]:
+    print("Dutch?")
