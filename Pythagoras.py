db69ee571c8b4c74eb7cd800bb874ae32ea28512
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-10-22 08:59:35 +0900

    Create Pythagoras.py

---
+++ Pythagoras.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+Author: Yoshiyosi
+"""
+import math
+
+def pythag(a,b):
+    c2=a**2+b**2
+    c=math.sqrt(c2)
+    return c
+    
+def pythag2(a,b):
+    if a>=b:
+        c2=a**2-b**2
+        c=math.sqrt(c2)
+        return c
+    if a<b:
+        c2=b**2-a**2
+        c=math.sqrt(c2)
+        return c
+
+print(pythag(3,4))
+print(pythag(math.sqrt(2),math.sqrt(2)))
+print(pythag2(5,4))
+print(pythag2(4,5))
