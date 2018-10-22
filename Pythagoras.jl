8c2d230e1103e5de9a6ec901f862c15d522cc34a
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-10-22 09:14:07 +0900

    Create Pythagoras.jl

---
+++ Pythagoras.jl
@@ -0,0 +1,20 @@
+"""
+Author: Yoshiyosi
+"""
+function pythag(a,b)
+    return return √(a**2+b**2)
+end
+
+function pythag2(a,b)
+    if a>=b
+        return √(a**2-b**2)
+    end
+    if a<b
+        return √(b**2-a**2)
+    end
+end
+
+println(pythag(3,4))
+println(pythag(√(2),√(2)))
+println(pythag2(5,4))
+println(pythag2(4,5))
