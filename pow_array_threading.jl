571b17f4136a6993e403e5c66c42d76392b95d83
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-10-24 11:44:45 +0900

    Add files via upload

---
+++ pow_array_threading.jl
@@ -0,0 +1,14 @@
+"""
+Created on Wed Oct 24 11:29:07 2018
+
+@author: user
+"""
+function pow_array(n)
+    n^2
+end
+
+result=zeros(Float64,10)
+Threads.@threads for i=1:10
+    result[i]=pow_array(i)
+end
+println(result)
