c5497af51546d82ae24d956534f4d1e5bcefdc34
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-11-15 00:19:29 +0900

    Add files via upload

---
+++ channel.jl
@@ -0,0 +1,16 @@
+﻿"""
+Author: Yosiyoshi
+"""
+
+function channel_test()
+	Channel(ctype=String, csize=23) do channel::Channel{String}
+		i = 1
+		while i > 0
+			sl, i = rand([("A", i+1), ("B", +(i<5))])
+			put!(channel, sl)
+		end
+	put!(channel, "CDEFG")
+	end
+end
+
+channel_test()
