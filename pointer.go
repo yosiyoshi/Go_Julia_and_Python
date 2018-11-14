8194250648d66d68bd60620520a1cc5111fa2263
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-11-14 23:12:25 +0900

    Add files via upload

---
+++ pointer.go
@@ -0,0 +1,18 @@
+﻿//author: yosiyoshi
+package main
+import "fmt"
+
+type Event struct {
+	Name string
+	Year int
+}
+
+func main() {
+	var ev *Event
+
+	ev = &Event{
+		Name: "Fall of Byzantine",
+		Year: 1453,
+	}
+	fmt.Printf("Event input: %ev", ev)
+}
