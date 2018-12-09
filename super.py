a4921f762b06c2239e3777de6480140d9f01a151
Yoshiよし <38394135+yosiyoshi@users.noreply.github.com>
2018-12-10 00:29:31 +0900

    Add files via upload

---
+++ super.py
@@ -0,0 +1,16 @@
+﻿# coding: utf-8
+# Your code here!
+class MyClass:
+    def __init__(self, name):
+        self.name=name
+    def say(self):
+        print(self.name)
+class UltraClass(MyClass):
+    def __init__(self, name, year):
+        super(UltraClass, self).__init__(name)
+        self.year=year
+    def say(self):
+        print(self.name, "was born in", self.year)
+
+uc=UltraClass("John", 1990)
+uc.say()
